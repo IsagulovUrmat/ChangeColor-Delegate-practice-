dd708ef601a4263b19a1c9348c4edb9399b4868a
isagulovurmat <falone641234@gmail.com>
2025-01-28 14:54:05 +0600

    Initial Commit

---
+++ ChangeColors(Delegate practice)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChangeColors(Delegate practice)
+//
+//  Created by sunflow on 28/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
